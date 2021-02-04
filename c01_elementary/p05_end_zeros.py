6dcdbebce7ba50985c702c009c0b8295758a1765
ZpRoc <PengZheng@188.com>
2021-02-04 15:23:24 +0800

    Initial submission of the Elementray chapter on the Checkiio game.

---
+++ c01_elementary/p05_end_zeros.py
@@ -0,0 +1,39 @@
+# p05_end_zeros.py
+#   How many zeros are at the end?
+#   (string)
+
+
+# Try to find out how many zeros a given number has at the end.
+
+# Input: A positive Int
+# Output: An Int.
+
+
+def end_zeros(num: int) -> int:
+    # your code here
+    n = str(num)
+    return len(n) - len(n.strip('0'))
+
+
+def end_zeros_1(num: int) -> int:
+    cnt = len(str(num))
+    while cnt > 0:
+        if num % (10**cnt) == 0:
+            break
+        else:
+            cnt = cnt - 1
+    return cnt
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(end_zeros(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert end_zeros(0) == 1
+    assert end_zeros(1) == 0
+    assert end_zeros(10) == 1
+    assert end_zeros(101) == 0
+    assert end_zeros(245) == 0
+    assert end_zeros(100100) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ c01_elementary/p09_replace_first.py
@@ -0,0 +1,39 @@
+# p09_replace_first.py
+#   The first element should become the last one
+#   (array, numbers)
+
+
+# In a given list the first element should become the last one. 
+# An empty list or list with only one element should stay the same.
+
+# Input: List.
+# Output: Iterable.
+
+
+from typing import Iterable
+
+
+def replace_first(items: list) -> Iterable:
+    # your code here
+    if items:
+        items.append(items.pop(0))
+    return items
+
+
+def replace_first_1(items: list) -> Iterable:
+    if len(items) > 0:
+        items.append(items[0])
+        return items[1:]
+    else:
+        return items
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(replace_first([1, 2, 3, 4])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(replace_first([1, 2, 3, 4])) == [2, 3, 4, 1]
+    assert list(replace_first([1])) == [1]
+    assert list(replace_first([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
