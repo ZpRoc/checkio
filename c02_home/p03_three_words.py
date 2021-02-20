5a0bd71219fc3fff25d08b908c6cd67cf7dd65b4
ZpRoc <zheng_roc@163.com>
2021-02-20 19:34:15 +0800

    Submission c02 p03_three_words and p04_right_to_left.

--- c02_home/p03_three_words.py
+++ c02_home/p03_three_words.py
@@ -6,19 +6,51 @@
 
 # ---------------------------------------------------------------- #
 
+# Let's teach the Robots to distinguish words and numbers.
 
+# You are given a string with words and numbers separated by 
+# whitespaces (one space). The words contains only letters. 
+# You should check if the string contains three words in succession. 
+# For example, the string "start 5 one two three 7 end" contains 
+# three words in succession.
+
+# Input: A string with words.
+# Output: The answer as a boolean.
+# Precondition: The input contains words and/or numbers. 
+#               There are no mixed words (letters and digits combined).
+#               0 < len(words) < 100
 
 
 # ---------------------------------------------------------------- #
 # ---------------------------------------------------------------- #
 # ---------------------------------------------------------------- #
 
+def checkio(words: str) -> bool:
+    val = [i.isdigit() for i in words.split()]
+    return 'False, False, False' in ''.join(str(val))
 
+
+def checkio_1(words: str) -> bool:
+    cnt = 0
+    for word in words.split(' '):
+        cnt = cnt + 1 if not word.isdigit() else 0
+        if cnt > 2:
+            return True  
+    return False
 
 
 # ---------------------------------------------------------------- #
 # ---------------------------------------------------------------- #
 # ---------------------------------------------------------------- #
 
-
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio("Hello World hello"))
+    
+    assert checkio("Hello World hello") == True, "Hello"
+    assert checkio("He is 123 man") == False, "123 man"
+    assert checkio("1 2 3 4") == False, "Digits"
+    assert checkio("bla bla bla bla") == True, "Bla Bla"
+    assert checkio("Hi") == False, "Hi"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
 
